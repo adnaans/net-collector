ef0e4fc03fa8fc3ce8c7c04dbebb9cbc9ade724c
Adnaan Sachidanandan <adnaan@onlab.us>
2017-07-25 18:23:05 -0700

    addedded npm modules and fixed server. added script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var metric = 30;
+if(metric >= 25){
+    console.log("Get back to work!");
+    //Make TV screen go off
+    document.getElementById("msg").innerHTML = "GET BACK TO WORK!";
+    document.getElementById("bg").style.backgroundColor =	"#FF0000";
+}
--- server.js
+++ server.js
@@ -1,8 +1,45 @@
 //Get metrics from the client continuously
 //Store as var metric
-if(metric >= 25){
-  console.log("Get back to work!");
-  //Make TV screen go off
-  document.getElementById("msg").innerHTML = "GET BACK TO WORK!";
-  document.getElementById("bg").style.backgroundColor =	"#FF0000";
-}
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+
+var mimeTypes = {
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "png": "image/png",
+    "js": "text/javascript",
+    "css": "text/css"};
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+      var uri = url.parse(request.url).pathname;
+      console.log(uri);
+      var filename = path.join(process.cwd(), uri);
+      fs.exists(filename, function(exists){
+        if(!exists || uri=="/") {
+          response.writeHeader(200, {"Content-Type": "text/html"});  
+          response.write(html);  
+          response.end();
+          return
+        }
+        var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
+        response.writeHead(200, mimeType);
+
+        console.log("success");
+        var fileStream = fs.createReadStream(filename);
+        fileStream.pipe(response);
+
+      });
+      //   response.writeHeader(200, {"Content-Type": "text/html"});  
+      //   response.write(html);  
+      //   response.end();  
+    }).listen(8000);
+});
+
+

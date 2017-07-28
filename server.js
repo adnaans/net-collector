dad9ae292644c460b3d05c4459e31a4629c10554
Adnaan Sachidanandan <adnaan@onlab.us>
2017-07-28 15:23:29 -0700

    added completed server without styling/css (need to do later) and removed commented code

--- server.js
+++ server.js
@@ -13,50 +13,11 @@ var mimeTypes = {
     "png": "image/png",
     "js": "text/javascript",
     "css": "text/css"};
-/*
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-      var uri = url.parse(request.url).pathname;
-      console.log(uri);
-      var filename = path.join(process.cwd(), uri);
-      fs.exists(filename, function(exists){
-        if(!exists || uri=="/") {
-          response.writeHeader(200, {"Content-Type": "text/html"});  
-          response.write(html);  
-          response.end();
-          return
-        }
-        var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
-        response.writeHead(200, mimeType);
-
-        console.log("success");
-        var fileStream = fs.createReadStream(filename);
-        fileStream.pipe(response);
-
-      });
-      //   response.writeHeader(200, {"Content-Type": "text/html"});  
-      //   response.write(html);  
-      //   response.end();  
-    }).listen(8000);
-});
-
-*/
 
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
-
-// app.get('/', function(req, res){
-// res.sendFile(__dirname + '/index.html');
-// });
-
-// app.get('/', function (req, res) {
-  // res.send('Hello World!')
-// })
 
 app.use( bodyParser.json() );
 
@@ -65,36 +26,16 @@ app.post('/post', function (req, res) {
   if(req.body['ptg']>12){
     io.emit('backtowork');
   }
-  else{
+  else if(req.body['ptg']<=12){
     io.emit('keepworking');
+  }
+  else{
+    console.log("error");
   }
   res.send('Got a POST request')
 })
 
-// app.put('/user', function (req, res) {
-//   res.send('Got a PUT request at /user')
-// })
-
-// app.delete('/user', function (req, res) {
-//   res.send('Got a DELETE request at /user')
-// })
-
 app.use(express.static(__dirname))
-
-
-// app.get('/Users/adnaansachidanandan/Projects/net-collector', function(req, res){
-//   res.sendFile(__dirname + '/index.html');
-// });
-
-// io.on('connection', function(socket){
-//   console.log('a user connected');
-//   socket.on('disconnect', function(){
-//     console.log('user disconnected');
-//   });
-//   socket.on('chat message', function(msg){
-//     io.emit('chat message', msg);
-//   });
-// });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');

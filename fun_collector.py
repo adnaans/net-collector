85558bc1de69068061be8c4ebd379a0ba12ecd3c
sarah feng <sarah@onlab.us>
2017-08-03 14:52:52 -0700

    trying to get second probe up

--- fun_collector.py
+++ fun_collector.py
@@ -100,14 +100,14 @@ class CollectorServicer(gnmi_pb2_grpc.gNMIServicer):
         queues.append(q)
 
         iter1 = request_iterator
-        #iter2 = copy.deepcopy(iter1)
+        iter2 = copy.deepcopy(iter1) #this failed, horribly :(
 
         #start streaming
         stubs = [stub1, stub2]
-        #iters = [iter1, iter2]
+        iters = [iter1, iter2]
         threads = []
         for stub in stubs:
-            t = threading.Thread(target=self.stream, args=(stub, request_iterator)) #iters[stubs.index(stub)]
+            t = threading.Thread(target=self.stream, args=(stub, iters[stubs.index(stub)])) #iters[stubs.index(stub)]
             threads.append(t) #is this even needed bro
             t.start()
         processingT = threading.Thread(target=self.processThatQ)
